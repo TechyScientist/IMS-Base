6fecad673ee1e3fffcfe83dc64e0cb032b8f01a6
Johnny Console <johnny.console@hotmail.com>
2025-09-16 14:05:32 -0400

    Add User Sign In POST API

---
+++ ims-restful/src/main/java/com/johnnyconsole/ims/restful/UserAPI.java
@@ -0,0 +1,44 @@
+package com.johnnyconsole.ims.restful;
+
+import com.johnnyconsole.ims.persistence.User;
+import com.johnnyconsole.ims.persistence.interfaces.UserDaoLocal;
+
+import javax.ejb.EJB;
+import javax.enterprise.context.RequestScoped;
+import javax.ws.rs.*;
+import javax.ws.rs.core.Response;
+
+import static javax.ws.rs.core.MediaType.*;
+
+@Path("/user")
+@RequestScoped
+public class UserAPI {
+
+    @EJB private UserDaoLocal userDao;
+
+    @POST
+    @Path("/signin")
+    @Produces(APPLICATION_JSON)
+    public Response signin(@FormParam("username") String username, @FormParam("password") String password) {
+        User user = userDao.getUser(username);
+        String response = "{\n";
+        if (user == null) {
+            response += "\t\"status\": 404\n";
+            response += "\t\"message\": \"User '" + username + "' not found\"\n";
+        }
+        else if(user.verifyPassword(password)) {
+            response += "\t\"status\": 200\n";
+            response += "\t\"user\": {\n";
+            response += "\t\t\"username\": \"" + username + "\"\n";
+            response += "\t\t\"name\": \"" + user.name + "\"\n";
+            response += "\t\t\"access\": " + user.accessLevel + "\n";
+            response += "\t}";
+        }
+        else {
+            response += "\t\"status\": 401\n";
+            response += "\t\"message\": \"Incorrect password for user '" + username + "'\"\n";
+        }
+
+        return Response.ok(response + "\n}").build();
+    }
+}
