a4022253342c0d493e6a6b6058faddd33759bbfe
Johnny Console <johnny.console@hotmail.com>
2025-09-16 14:05:05 -0400

    Add JAX-RS Activator class

---
+++ ims-restful/src/main/java/com/johnnyconsole/ims/restful/JaxRsActivator.java
@@ -0,0 +1,9 @@
+package com.johnnyconsole.ims.restful;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class JaxRsActivator extends Application {
+    //Intentionally Left Blank
+}
